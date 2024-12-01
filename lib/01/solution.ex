21076b593f991478d4184b2562b1686e31c9ae86
Jared Wyce <wycejared@gmail.com>
2024-12-01 09:43:21 -0500

    📜😭 day 1: Historian Hysteria

--- lib/01/solution.ex
+++ lib/01/solution.ex
@@ -4,16 +4,52 @@ defmodule Aoc.D1 do
 
     case File.read(file_path) do
       {:ok, content} ->
-        IO.puts(content)
+        # IO.puts(content)
+
+        lines = String.split(content, "\n")
+
+        groups =
+          for line <- lines,
+              line != "",
+              do:
+                line |> String.split(~r/\s+/) |> Enum.map(&String.to_integer/1) |> List.to_tuple()
+
+        group_one = groups |> Enum.map(&elem(&1, 0)) |> Enum.sort()
+        group_two = groups |> Enum.map(&elem(&1, 1)) |> Enum.sort()
+
+        Enum.zip(group_one, group_two)
+        |> Enum.map(fn {a, b} -> abs(a - b) end)
+        |> Enum.sum()
 
       {:error, reason} ->
         IO.puts("Failed to read file: #{reason}")
     end
-
-    :a
   end
 
   def b do
-    :b
+    file_path = Path.join(__DIR__, "input.txt")
+
+    case File.read(file_path) do
+      {:ok, content} ->
+        # IO.puts(content)
+
+        lines = String.split(content, "\n")
+
+        groups =
+          for line <- lines,
+              line != "",
+              do:
+                line |> String.split(~r/\s+/) |> Enum.map(&String.to_integer/1) |> List.to_tuple()
+
+        group_one = groups |> Enum.map(&elem(&1, 0)) |> Enum.sort()
+        frequencies = groups |> Enum.map(&elem(&1, 1)) |> Enum.frequencies()
+
+        group_one
+        |> Enum.map(&(&1 * Map.get(frequencies, &1, 0)))
+        |> Enum.sum()
+
+      {:error, reason} ->
+        IO.puts("Failed to read file: #{reason}")
+    end
   end
 end
