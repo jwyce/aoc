2b860c268984d7f63c475ebffebfea5face216b1
Jared Wyce <wycejared@gmail.com>
2025-12-01 09:31:03 -0500

    day 1 - 🐫 ocaml my caml

--- 01/solution.ml
+++ 01/solution.ml
@@ -10,9 +10,57 @@ let read_input () =
   in
   read_lines []
 
-let part1 _lines = 0
+type direction = Left | Right
 
-let part2 _lines = 0
+let rotations =
+  List.map (fun s ->
+      Scanf.sscanf s "%c%d" (fun c n ->
+          let dir =
+            match c with
+            | 'R' -> Right
+            | 'L' -> Left
+            | _ -> failwith "Expected R or L"
+          in
+          (dir, n)))
+
+let num_zeros rotations =
+  let count, _ =
+    List.fold_left
+      (fun (acc, dial) (dir, n) ->
+        let delta = match dir with Left -> -n | Right -> n in
+        let new_pos = (dial + delta) mod 100 in
+        let new_pos = if new_pos < 0 then new_pos + 100 else new_pos in
+        let acc = if new_pos = 0 then acc + 1 else acc in
+        (acc, new_pos))
+      (0, 50) rotations
+  in
+  count
+
+let num_clicks rotations =
+  let count, _ =
+    List.fold_left
+      (fun (acc, dial) (dir, n) ->
+        let delta = match dir with Left -> -n | Right -> n in
+        let new_pos = (dial + delta) mod 100 in
+        let new_pos = if new_pos < 0 then new_pos + 100 else new_pos in
+        let first_zero =
+          if dial = 0 then 100 else if dir = Right then 100 - dial else dial
+        in
+        let acc =
+          if first_zero <= n then acc + (((n - first_zero) / 100) + 1) else acc
+        in
+        (acc, new_pos))
+      (0, 50) rotations
+  in
+  count
+
+let part1 _lines =
+  let rotations = rotations _lines in
+  num_zeros rotations
+
+let part2 _lines =
+  let rotations = rotations _lines in
+  num_clicks rotations
 
 let () =
   let lines = read_input () in
