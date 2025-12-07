5399ad9ee7b7aafd3114f1b2e8a6bfe3deb39b3f
Jared Wyce <wycejared@gmail.com>
2025-12-06 23:57:34 -0500

    day 3: 🪫 lobby (ocaml)

--- 03/solution.ml
+++ 03/solution.ml
@@ -1,20 +1,45 @@
-let read_input () =
-  let ic = open_in "03/input.txt" in
-  let rec read_lines acc =
-    try
-      let line = input_line ic in
-      read_lines (line :: acc)
-    with End_of_file ->
-      close_in ic;
-      List.rev acc
-  in
-  read_lines []
+open Base
+open Stdio
 
-let part1 _lines = 0
+let read_input () = In_channel.read_lines "03/input.txt"
 
-let part2 _lines = 0
+(* brute force *)
+let part1 lines =
+  lines
+  |> List.sum
+       (module Int)
+       ~f:(fun line ->
+         let bank = String.to_list line |> List.map ~f:Char.get_digit_exn in
+         let indexed = List.mapi bank ~f:(fun i d -> (i, d)) in
+         List.cartesian_product indexed indexed
+         |> List.filter_map ~f:(fun ((i, a), (j, b)) ->
+             if i < j then Some ((a * 10) + b) else None)
+         |> List.max_elt ~compare:Int.compare
+         |> Option.value ~default:0)
+
+(* greedy pick largest digit while I still have room *)
+let part2 lines =
+  lines
+  |> List.sum
+       (module Int)
+       ~f:(fun line ->
+         let bank = String.to_array line |> Array.map ~f:Char.get_digit_exn in
+         let n = Array.length bank in
+         let rec pick start needed acc =
+           if needed = 0 then acc
+           else
+             let end_ = n - needed in
+             let best_i, best_d =
+               List.range start (end_ + 1)
+               |> List.map ~f:(fun i -> (i, bank.(i)))
+               |> List.max_elt ~compare:(fun (_, a) (_, b) -> Int.compare a b)
+               |> Option.value_exn
+             in
+             pick (best_i + 1) (needed - 1) ((acc * 10) + best_d)
+         in
+         pick 0 12 0)
 
 let () =
   let lines = read_input () in
-  Printf.printf "Part 1: %d\n" (part1 lines);
-  Printf.printf "Part 2: %d\n" (part2 lines)
+  printf "Part 1: %d\n" (part1 lines);
+  printf "Part 2: %d\n" (part2 lines)
